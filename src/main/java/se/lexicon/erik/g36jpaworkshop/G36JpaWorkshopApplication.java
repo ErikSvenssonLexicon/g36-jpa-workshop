7d2457bf4ec3dfc6ed6b960784cbbde725512391
Erik Svensson <eosvensson@gmail.com>
2021-06-24 09:52:38 +0200

    Part 1: Project Setup

---
+++ src/main/java/se/lexicon/erik/g36jpaworkshop/G36JpaWorkshopApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.erik.g36jpaworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G36JpaWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G36JpaWorkshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/erik/g36jpaworkshop/G36JpaWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.erik.g36jpaworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G36JpaWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
